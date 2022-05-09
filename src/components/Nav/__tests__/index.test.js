1588281b4de3f7ee309bad72f98f2f03a438ad54
thisisjona <thisisjona@gmail.com>
2022-05-08 23:03:44 -0500

    contact component and tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -4,15 +4,36 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+  { name: 'portraits', description: 'Portriats of people in my life'}
+]
+
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContectSelected = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
   it('renders', () => {
-    render(<Nav />);
+    render(<Nav
+      categories = {categories}
+      setCurrentCategory = {mockSetCurrentCategory}
+      currentCategory = {mockCurrentCategory}
+      contactSelected = {mockContactSelected}
+      setContactSelected = {mockSetContectSelected}
+      />);
   });
 
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Nav />);
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Nav
+      categories = {categories}
+      setCurrentCategory = {mockSetCurrentCategory}
+      currentCategory = {mockCurrentCategory}
+      contactSelected = {mockContactSelected}
+      setContactSelected = {mockSetContectSelected}
+      />);
 
     expect(asFragment()).toMatchSnapshot();
   });
@@ -20,7 +41,13 @@ describe('Nav component', () => {
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-    const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(<Nav 
+      categories = {categories}
+      setCurrentCategory = {mockSetCurrentCategory}
+      currentCategory = {mockCurrentCategory}
+      contactSelected = {mockContactSelected}
+      setContactSelected = {mockSetContectSelected}
+      />);
 
     expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
@@ -28,7 +55,13 @@ describe('emoji is visible', () => {
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
-    const { getByTestId } = render(<Nav />);
+    const { getByTestId } = render(<Nav 
+      categories = {categories}
+      setCurrentCategory = {mockSetCurrentCategory}
+      currentCategory = {mockCurrentCategory}
+      contactSelected = {mockContactSelected}
+      setContactSelected = {mockSetContectSelected}
+      />);
 
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
